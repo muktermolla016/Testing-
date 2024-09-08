8ed19c492e2d60ec7c13b0860c74a8496be01e6e
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-09-08 11:52:41 +0530

    Update thumbnails.py

--- SHUKLAMUSIC/utils/thumbnails.py
+++ SHUKLAMUSIC/utils/thumbnails.py
@@ -1,16 +1,12 @@
 import os
 import re
-import textwrap
-
 import aiofiles
 import aiohttp
-import numpy as np
-
-from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFont
+from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
+from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
-
-from config import YOUTUBE_IMG_URL
 from SHUKLAMUSIC import app
+from config import YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
