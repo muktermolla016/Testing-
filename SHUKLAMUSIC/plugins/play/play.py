ff0a86c5b23de371951d9bda511b6789b2dba7c5
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2025-10-31 01:48:18 +0530

    Update play.py

--- SHUKLAMUSIC/plugins/play/play.py
+++ SHUKLAMUSIC/plugins/play/play.py
@@ -22,7 +22,7 @@ from SHUKLAMUSIC.utils.inline import (
 )
 from SHUKLAMUSIC.utils.logger import play_logs
 from SHUKLAMUSIC.utils.stream.stream import stream
-from config import BANNED_USERS, lyrical, AMBOT
+from config import BANNED_USERS, lyrical
 
 
 @app.on_message(
@@ -44,7 +44,7 @@ async def play_commnd(
     fplay,
 ):
     mystic = await message.reply_text(
-        _["play_2"].format(channel) if channel else random.choice(AMBOT)
+        _["play_2"].format(channel) if channel else _["play_1"]
     )
     plist_id = None
     slider = None
@@ -290,7 +290,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await SHUKLA.stream_call(url)
+                await PURVI.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
@@ -456,7 +456,7 @@ async def play_music(client, CallbackQuery, _):
     except:
         pass
     mystic = await CallbackQuery.message.reply_text(
-                _["play_2"].format(channel) if channel else random.choice(AMBOT)
+        _["play_2"].format(channel) if channel else _["play_1"]
     )
     try:
         details, track_id = await YouTube.track(vidid, True)
@@ -503,8 +503,8 @@ async def play_music(client, CallbackQuery, _):
     return await mystic.delete()
 
 
-@app.on_callback_query(filters.regex("SHUKLAmousAdmin") & ~BANNED_USERS)
-async def SHUKLAmous_check(client, CallbackQuery):
+@app.on_callback_query(filters.regex("PURVImousAdmin") & ~BANNED_USERS)
+async def PURVImous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
             "» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ :\n\nᴏᴘᴇɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴇᴛᴛɪɴɢs.\n-> ᴀᴅᴍɪɴɪsᴛʀᴀᴛᴏʀs\n-> ᴄʟɪᴄᴋ ᴏɴ ʏᴏᴜʀ ɴᴀᴍᴇ\n-> ᴜɴᴄʜᴇᴄᴋ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴs.",
@@ -514,7 +514,7 @@ async def SHUKLAmous_check(client, CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("SHUKLAPlaylists") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("DAXXPlaylists") & ~BANNED_USERS)
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
@@ -543,7 +543,7 @@ async def play_playlists_command(client, CallbackQuery, _):
     except:
         pass
     mystic = await CallbackQuery.message.reply_text(
-                _["play_2"].format(channel) if channel else random.choice(AMBOT)
+        _["play_2"].format(channel) if channel else _["play_1"]
     )
     videoid = lyrical.get(videoid)
     video = True if mode == "v" else None
