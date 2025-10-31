0f8feae2d1daa11eafc9a7b0f21032248d2886c0
muktermolla016 <muktermolla016@gmail.com>
2025-10-31 23:11:01 +0600

    Update UPSTREAM_REPO URL to remove .git suffix

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/muktermolla016/Testing.git",
+    "https://github.com/muktermolla016/Testing",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
